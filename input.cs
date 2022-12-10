2611bb73068ee4e6aab6feb927fba9edeecb1c7c
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-12-10 19:25:49 +0530

    Input getting as Integer

---
+++ input.cs
@@ -0,0 +1,12 @@
+using System;
+class HelloWorld {
+ public static  void Main() {
+    int a,b,c;
+    Console.WriteLine("Enter two nums :");
+    a=int.Parse(Console.ReadLine());
+    b=int.Parse(Console.ReadLine());
+    c=a+b;
+    Console.WriteLine("The total is :"+c);
+    
+    }
+}

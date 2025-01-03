3cd805d1ad53b8586ccd0255f01a7f542c168236
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-01-03 15:34:04 +0530

    Update input.cs

--- input.cs
+++ input.cs
@@ -3,10 +3,19 @@ class HelloWorld {
  public static  void Main() {
     int a,b,c;
     Console.WriteLine("Enter two nums :");
+  // Input method-1
     a=int.Parse(Console.ReadLine());
     b=int.Parse(Console.ReadLine());
     c=a+b;
+    
     Console.WriteLine("The total is :"+c);
+
+  // Input method-2
+
+  int i = Convert.ToInt32(Console.ReadLine());
+double d = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("I " + i);
+Console.WriteLine("D is " + d);
     
     }
 }
